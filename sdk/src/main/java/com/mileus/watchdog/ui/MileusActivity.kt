ce5a3b2144458d9e09071ff11746a7ade32e3af9
Karel Patlejch <patlejch.karel@gmail.com>
2020-06-30 13:52:45 +0100

    added market validation activity

---
+++ sdk/src/main/java/com/mileus/watchdog/ui/MileusActivity.kt
@@ -0,0 +1,187 @@
+package com.mileus.watchdog.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.res.Resources
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.webkit.*
+import androidx.appcompat.app.AppCompatActivity
+import com.mileus.sdk.R
+import com.mileus.watchdog.*
+import com.mileus.watchdog.data.Location
+import kotlinx.android.synthetic.main.activity_mileus_watchdog.*
+
+abstract class MileusActivity : AppCompatActivity() {
+
+    companion object {
+        const val URL_DEVELOPMENT = "https://mileus.spacek.now.sh/"
+        const val URL_STAGING = "https://watchdog-web-stage.mileus.com/"
+        const val URL_PRODUCTION = "https://watchdog-web.mileus.com/"
+    }
+
+    protected var origin: Location? = null
+    protected var destination: Location? = null
+    protected lateinit var token: String
+    protected var partnerName = MileusWatchdog.partnerName
+    protected var environment = MileusWatchdog.environment
+
+    protected val baseUrl: String
+        get() = when (environment) {
+            MileusWatchdog.ENV_PRODUCTION -> URL_PRODUCTION
+            MileusWatchdog.ENV_STAGING -> URL_STAGING
+            else -> URL_DEVELOPMENT
+        }
+
+    protected abstract val mode: String
+
+    protected val language: String
+        get() {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+                Resources.getSystem().configuration.locales[0].language
+            else
+                Resources.getSystem().configuration.locale.language
+        }
+
+    protected var webview: WebView? = null
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            webview?.post { webview?.setNetworkAvailable(true) }
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            webview?.post { webview?.setNetworkAvailable(false) }
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled", "SourceLockedOrientationActivity")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mileus_watchdog)
+        setTheme(R.style.MileusTheme)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        mileus_toolbar.setNavigationOnClickListener { finish() }
+
+        origin = intent.extras?.origin
+        destination = intent.extras?.destination
+        token = intent.extras?.token ?: throw IllegalStateException("Missing access token.")
+        partnerName = MileusWatchdog.partnerName
+
+        savedInstanceState?.let {
+            it.origin?.let { origin = it }
+            it.destination?.let { destination = it }
+        }
+
+        val progressBar = mileus_progress
+        webview = mileus_webview
+        val errorLayout = mileus_error
+        webview?.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    return request?.url?.host?.contains(baseUrl)?.not() ?: false
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    if (errorLayout.visibility != View.VISIBLE) {
+                        webview?.visibility = View.VISIBLE
+                    }
+                    progressBar.visibility = View.GONE
+                }
+
+                override fun onReceivedError(
+                    view: WebView?,
+                    request: WebResourceRequest?,
+                    error: WebResourceError?
+                ) {
+                    super.onReceivedError(view, request, error)
+                    if (request?.isForMainFrame != true) {
+                        return
+                    }
+                    webview?.visibility = View.INVISIBLE
+                    errorLayout.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        mileus_error_retry.setOnClickListener {
+            loadWeb()
+        }
+
+        if (savedInstanceState == null) {
+            loadWeb()
+        } else {
+            webview?.restoreState(savedInstanceState)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mileus_webview.saveState(outState)
+
+        outState.let {
+            it.origin = origin
+            it.destination = destination
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+            .registerNetworkCallback(NetworkRequest.Builder().build(), networkCallback)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+            .unregisterNetworkCallback(networkCallback)
+    }
+
+    override fun onDestroy() {
+        webview = null
+        super.onDestroy()
+    }
+
+    private fun loadWeb() {
+        mileus_progress.visibility = View.VISIBLE
+        mileus_error.visibility = View.GONE
+        mileus_webview.loadUrl(buildUrl())
+    }
+
+    private fun buildUrl() = Uri.parse(baseUrl)
+        .buildUpon()
+        .appendQueryParameter("partner_name", partnerName)
+        .appendQueryParameter("environment", environment)
+        .appendQueryParameter("access_token", token)
+        .appendQueryParameter("language", language)
+        .appendQueryParameter("platform", "android")
+        .appendQueryParameter("mode", mode).run {
+            origin?.let {
+                appendQueryParameter("origin_lat", it.latitude.toString())
+                appendQueryParameter("origin_lon", it.longitude.toString())
+                appendQueryParameter("origin_address", it.address)
+            }
+            destination?.let {
+                appendQueryParameter("destination_lat", it.latitude.toString())
+                appendQueryParameter("destination_lon", it.longitude.toString())
+                appendQueryParameter("destination_address", it.address)
+            }
+            build().toString()
+        }
+
+    protected fun String.sanitize() = replace("\\", "\\\\").replace("'", "\\'")
+}
--- sdk/src/main/java/com/mileus/watchdog/ui/MileusWatchdogActivity.kt
+++ sdk/src/main/java/com/mileus/watchdog/ui/MileusWatchdogActivity.kt
@@ -1,49 +1,22 @@
 package com.mileus.watchdog.ui
 
-import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.content.pm.ActivityInfo
-import android.content.res.Resources
-import android.net.*
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.webkit.*
-import androidx.appcompat.app.AppCompatActivity
-import com.mileus.sdk.*
+import android.webkit.JavascriptInterface
 import com.mileus.watchdog.*
-import com.mileus.watchdog.data.Location
-import com.mileus.watchdog.location
-import com.mileus.watchdog.origin
-import com.mileus.watchdog.token
 import kotlinx.android.synthetic.main.activity_mileus_watchdog.*
 
-class MileusWatchdogActivity : AppCompatActivity() {
+class MileusWatchdogActivity : MileusActivity() {
 
     companion object {
-        const val URL_DEVELOPMENT = "https://mileus.spacek.now.sh/"
-        const val URL_STAGING = "https://watchdog-web-stage.mileus.com/"
-        const val URL_PRODUCTION = "https://watchdog-web.mileus.com/"
         const val REQUEST_CODE_ORIGIN_SEARCH = 2501
         const val REQUEST_CODE_DESTINATION_SEARCH = 2502
         const val SEARCH_TYPE_ORIGIN = "origin"
         const val SEARCH_TYPE_DESTINATION = "destination"
     }
 
-    private var origin: Location? = null
-    private var destination: Location? = null
-    private lateinit var token: String
-    private var partnerName = MileusWatchdog.partnerName
-    private var environment = MileusWatchdog.environment
-
-    private val baseUrl: String
-        get() = when (environment) {
-            MileusWatchdog.ENV_PRODUCTION -> URL_PRODUCTION
-            MileusWatchdog.ENV_STAGING -> URL_STAGING
-            else -> URL_DEVELOPMENT
-        }
+    override val mode = "watchdog"
 
     private val originSearchIntent: Intent?
         get() = MileusWatchdog.originSearchActivityIntent?.updateExtras {
@@ -62,127 +35,10 @@ class MileusWatchdogActivity : AppCompatActivity() {
     private val taxiRideActivityIntent: Intent?
         get() = MileusWatchdog.taxiRideActivityIntent
 
-    private val language: String
-        get() {
-            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                Resources.getSystem().configuration.locales[0].language
-            else
-                Resources.getSystem().configuration.locale.language
-        }
-
-    private var webview: WebView? = null
-
-    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
-        override fun onAvailable(network: Network) {
-            super.onAvailable(network)
-            webview?.post { webview?.setNetworkAvailable(true) }
-        }
-
-        override fun onLost(network: Network) {
-            super.onLost(network)
-            webview?.post { webview?.setNetworkAvailable(false) }
-        }
-    }
-
-    @SuppressLint("SetJavaScriptEnabled", "SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mileus_watchdog)
-        setTheme(R.style.MileusTheme)
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
-        mileus_toolbar.setNavigationOnClickListener { finish() }
-
-        origin = intent.extras?.origin
-        destination = intent.extras?.destination
-        token = intent.extras?.token ?: throw IllegalStateException("Missing access token.")
-        partnerName = MileusWatchdog.partnerName
-
-        savedInstanceState?.let {
-            it.origin?.let { origin = it }
-            it.destination?.let { destination = it }
-        }
-
-        val progressBar = mileus_progress
-        webview = mileus_webview
-        val errorLayout = mileus_error
-        webview?.apply {
-            settings.javaScriptEnabled = true
-            webViewClient = object : WebViewClient() {
-                override fun shouldOverrideUrlLoading(
-                    view: WebView?,
-                    request: WebResourceRequest?
-                ): Boolean {
-                    return request?.url?.host?.contains(baseUrl)?.not() ?: false
-                }
-
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
-                    if (errorLayout.visibility != View.VISIBLE) {
-                        webview?.visibility = View.VISIBLE
-                    }
-                    progressBar.visibility = View.GONE
-                }
-
-                override fun onReceivedError(
-                    view: WebView?,
-                    request: WebResourceRequest?,
-                    error: WebResourceError?
-                ) {
-                    super.onReceivedError(view, request, error)
-                    if (request?.isForMainFrame != true) {
-                        return
-                    }
-                    webview?.visibility = View.INVISIBLE
-                    errorLayout.visibility = View.VISIBLE
-                }
-            }
-
-            addJavascriptInterface(this@MileusWatchdogActivity, "MileusNative")
-        }
-
-        mileus_error_retry.setOnClickListener {
-            loadWeb()
-        }
-
-        if (savedInstanceState == null) {
-            loadWeb()
-        } else {
-            webview?.restoreState(savedInstanceState)
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mileus_webview.saveState(outState)
-
-        outState.let {
-            it.origin = origin
-            it.destination = destination
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
-            .registerNetworkCallback(NetworkRequest.Builder().build(), networkCallback)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
-            .unregisterNetworkCallback(networkCallback)
-    }
-
-    override fun onDestroy() {
-        webview = null
-        super.onDestroy()
-    }
-
-    private fun loadWeb() {
-        mileus_progress.visibility = View.VISIBLE
-        mileus_error.visibility = View.GONE
-        mileus_webview.loadUrl(buildUrl())
+        webview?.addJavascriptInterface(this@MileusWatchdogActivity, "MileusNative")
     }
 
     private fun updateLocationsInJs() {
@@ -218,11 +74,13 @@ class MileusWatchdogActivity : AppCompatActivity() {
     fun openSearchScreen(searchType: String) {
         when (searchType) {
             SEARCH_TYPE_ORIGIN ->
-                startActivityForResult(originSearchIntent,
+                startActivityForResult(
+                    originSearchIntent,
                     REQUEST_CODE_ORIGIN_SEARCH
                 )
             SEARCH_TYPE_DESTINATION ->
-                startActivityForResult(destinationSearchIntent,
+                startActivityForResult(
+                    destinationSearchIntent,
                     REQUEST_CODE_DESTINATION_SEARCH
                 )
         }
@@ -255,27 +113,4 @@ class MileusWatchdogActivity : AppCompatActivity() {
 
         super.onActivityResult(requestCode, resultCode, data)
     }
-
-    private fun buildUrl() = Uri.parse(baseUrl)
-        .buildUpon()
-        .appendQueryParameter("partner_name", partnerName)
-        .appendQueryParameter("environment", environment)
-        .appendQueryParameter("access_token", token)
-        .appendQueryParameter("language", language)
-        .appendQueryParameter("platform", "android")
-        .appendQueryParameter("mode", "watchdog").run {
-            origin?.let {
-                appendQueryParameter("origin_lat", it.latitude.toString())
-                appendQueryParameter("origin_lon", it.longitude.toString())
-                appendQueryParameter("origin_address", it.address)
-            }
-            destination?.let {
-                appendQueryParameter("destination_lat", it.latitude.toString())
-                appendQueryParameter("destination_lon", it.longitude.toString())
-                appendQueryParameter("destination_address", it.address)
-            }
-            build().toString()
-        }
-
-    private fun String.sanitize() = replace("\\", "\\\\").replace("'", "\\'")
 }
