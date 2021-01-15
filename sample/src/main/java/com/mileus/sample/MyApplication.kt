4c85d36b7dc64f263942f9e26f26aa3cb53a679d
Karel Patlejch <patlejch.karel@gmail.com>
2021-01-15 13:36:17 +0000

    added a check for fg notification info in the scheduling activity

--- sample/src/main/java/com/mileus/sample/MyApplication.kt
+++ sample/src/main/java/com/mileus/sample/MyApplication.kt
@@ -1,14 +1,17 @@
 package com.mileus.sample
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import com.mileus.watchdog.MileusWatchdog
+import com.mileus.watchdog.data.NotificationInfo
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        MileusWatchdog.init("partnerName", MileusWatchdog.ENV_DEVELOPMENT)
 
         Intent(this, LocationSearchActivity::class.java).also {
             MileusWatchdog.destinationSearchActivityIntent = it
@@ -16,5 +19,23 @@ class MyApplication : Application() {
         }
 
         MileusWatchdog.taxiRideActivityIntent = Intent(this, TaxiRideActivity::class.java)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "mileus-sample-location-channel"
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                getString(R.string.notification_channel_name),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        MileusWatchdog.foregroundServiceNotificationInfo = NotificationInfo.Builder()
+            .setChannelId(channelId)
+            .setIconRes(R.drawable.ic_location_search)
+            .build()
     }
 }
--- sdk/src/main/java/com/mileus/watchdog/ui/MileusWatchdogSchedulingActivity.kt
+++ sdk/src/main/java/com/mileus/watchdog/ui/MileusWatchdogSchedulingActivity.kt
@@ -1,6 +1,8 @@
 package com.mileus.watchdog.ui
 
 import android.os.Bundle
+import android.util.Log
+import com.mileus.watchdog.MileusWatchdog
 
 class MileusWatchdogSchedulingActivity : MileusActivity() {
 
@@ -11,6 +13,13 @@ class MileusWatchdogSchedulingActivity : MileusActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         finishIfNotGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (MileusWatchdog.foregroundServiceNotificationInfo == null) {
+            val msg = "Cannot start activity, foreground service notification info has not been set up."
+            Log.e(this::class.simpleName, msg)
+            finish()
+        }
+
         super.onCreate(savedInstanceState)
     }
 
