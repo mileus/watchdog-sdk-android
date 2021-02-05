2d0de71cf5005bbb559c37067c647d0a0dcf98a6
Karel Patlejch <patlejch.karel@gmail.com>
2021-02-05 15:10:35 +0000

    moved permission-related methods from MileusActivity to MileusWatchdogSchedulingActivity

--- sdk/src/main/java/com/mileus/watchdog/ui/MileusActivity.kt
+++ sdk/src/main/java/com/mileus/watchdog/ui/MileusActivity.kt
@@ -1,6 +1,5 @@
 package com.mileus.watchdog.ui
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
@@ -14,12 +13,9 @@ import android.net.NetworkRequest
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.webkit.*
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.mileus.sdk.R
@@ -40,8 +36,6 @@ abstract class MileusActivity : AppCompatActivity() {
         const val SEARCH_TYPE_ORIGIN = "origin"
         const val SEARCH_TYPE_DESTINATION = "destination"
         const val SEARCH_TYPE_HOME = "home"
-
-        private const val REQUEST_CODE_SETTINGS_BG_LOCATION = 2504
     }
 
     protected var origin: Location? = null
@@ -101,8 +95,6 @@ abstract class MileusActivity : AppCompatActivity() {
         }
     }
 
-    private lateinit var requestPermission: ActivityResultLauncher<String>
-
     private var Bundle.toolbarText: String?
         get() = getString("toolbar")
         set(value) {
@@ -121,10 +113,6 @@ abstract class MileusActivity : AppCompatActivity() {
         setTheme(R.style.MileusTheme)
         setContentView(R.layout.activity_mileus_watchdog)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-
-        requestPermission = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { onRequestBackgroundLocationResult(it) }
 
         MileusWatchdog.assertInitialized()
 
@@ -260,9 +248,7 @@ abstract class MileusActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_CODE_SETTINGS_BG_LOCATION) {
-            onRequestBackgroundLocationSettingsResult()
-        } else if (resultCode == Activity.RESULT_OK) {
+        if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
                 REQUEST_CODE_ORIGIN_SEARCH -> {
                     origin = data?.extras?.location
@@ -325,88 +311,8 @@ abstract class MileusActivity : AppCompatActivity() {
     }
 
     @JavascriptInterface
-    fun verifyBackgroundLocationPermission() {
-        var permissionsGranted = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            permissionsGranted = permissionsGranted && ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        }
-        webview?.evaluateJavascript(
-            """
-                window.onVerifyBackgroundLocationPermissionResult({ 
-                    granted: "$permissionsGranted"
-                })
-            """.trimIndent(),
-            null
-        )
-    }
-
-    @JavascriptInterface
-    fun requestBackgroundLocationPermission() {
-        var permissionsGranted = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            permissionsGranted = permissionsGranted && ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        }
-
-        if (permissionsGranted) {
-            webview?.evaluateJavascript(
-                "window.onRequestBackgroundLocationPermissionResult({ granted: \"true\" })",
-                null
-            )
-        } else {
-            // at this point we can safely assume the OS version is >= Q
-            requestPermission.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-        }
-    }
-
-    @JavascriptInterface
     fun startLocationScanning() {
         MileusWatchdog.onSearchStartingSoon(this)
-    }
-
-    private fun onRequestBackgroundLocationResult(result: Boolean) {
-        var shouldShowRationale = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            shouldShowRationale = shouldShowRationale &&
-                    shouldShowRequestPermissionRationale(
-                        Manifest.permission.ACCESS_BACKGROUND_LOCATION
-                    )
-        }
-        if (!result && !shouldShowRationale) {
-            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                data = Uri.fromParts("package", packageName, null)
-            }
-            startActivityForResult(intent, REQUEST_CODE_SETTINGS_BG_LOCATION)
-        } else {
-            webview?.evaluateJavascript(
-                "window.onRequestBackgroundLocationPermissionResult({ granted: \"$result\" })",
-                null
-            )
-        }
-    }
-
-    private fun onRequestBackgroundLocationSettingsResult() {
-        var permissionsGranted = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            permissionsGranted = permissionsGranted && ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_BACKGROUND_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        }
-
-        webview?.evaluateJavascript(
-            """
-                window.onRequestBackgroundLocationPermissionResult({ 
-                    granted: "$permissionsGranted" 
-                })
-            """.trimIndent(),
-            null
-        )
     }
 
     protected fun updateLocationsInJs() {
--- sdk/src/main/java/com/mileus/watchdog/ui/MileusWatchdogSchedulingActivity.kt
+++ sdk/src/main/java/com/mileus/watchdog/ui/MileusWatchdogSchedulingActivity.kt
@@ -1,7 +1,17 @@
 package com.mileus.watchdog.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import android.webkit.JavascriptInterface
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.mileus.watchdog.MileusWatchdog
 
 /**
@@ -10,16 +20,27 @@ import com.mileus.watchdog.MileusWatchdog
  */
 class MileusWatchdogSchedulingActivity : MileusActivity() {
 
+    companion object {
+        private const val REQUEST_CODE_SETTINGS_BG_LOCATION = 2504
+    }
+
     override val mode = "watchdog_scheduling"
 
     override val defaultToolbarText: String
         get() = ""
 
+    private lateinit var requestPermission: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        finishIfNotGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        finishIfNotGranted(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        requestPermission = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { onRequestBackgroundLocationResult(it) }
 
         if (MileusWatchdog.foregroundServiceNotificationInfo == null) {
-            val msg = "Cannot start activity, foreground service notification info has not been set up."
+            val msg =
+                "Cannot start activity, foreground service notification info has not been set up."
             Log.e(this::class.simpleName, msg)
             finish()
         }
@@ -27,4 +48,91 @@ class MileusWatchdogSchedulingActivity : MileusActivity() {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE_SETTINGS_BG_LOCATION) {
+            onRequestBackgroundLocationSettingsResult()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    @JavascriptInterface
+    fun verifyBackgroundLocationPermission() {
+        var permissionsGranted = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionsGranted = permissionsGranted && ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+        webview?.evaluateJavascript(
+            """
+                window.onVerifyBackgroundLocationPermissionResult({ 
+                    granted: "$permissionsGranted"
+                })
+            """.trimIndent(),
+            null
+        )
+    }
+
+    @JavascriptInterface
+    fun requestBackgroundLocationPermission() {
+        var permissionsGranted = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionsGranted = permissionsGranted && ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+        if (permissionsGranted) {
+            webview?.evaluateJavascript(
+                "window.onRequestBackgroundLocationPermissionResult({ granted: \"true\" })",
+                null
+            )
+        } else {
+            // at this point we can safely assume the OS version is >= Q
+            requestPermission.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+    }
+
+    private fun onRequestBackgroundLocationResult(result: Boolean) {
+        var shouldShowRationale = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            shouldShowRationale = shouldShowRationale &&
+                    shouldShowRequestPermissionRationale(
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    )
+        }
+        if (!result && !shouldShowRationale) {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", packageName, null)
+            }
+            startActivityForResult(intent, REQUEST_CODE_SETTINGS_BG_LOCATION)
+        } else {
+            webview?.evaluateJavascript(
+                "window.onRequestBackgroundLocationPermissionResult({ granted: \"$result\" })",
+                null
+            )
+        }
+    }
+
+    private fun onRequestBackgroundLocationSettingsResult() {
+        var permissionsGranted = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionsGranted = permissionsGranted && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+        webview?.evaluateJavascript(
+            """
+                window.onRequestBackgroundLocationPermissionResult({ 
+                    granted: "$permissionsGranted" 
+                })
+            """.trimIndent(),
+            null
+        )
+    }
 }
